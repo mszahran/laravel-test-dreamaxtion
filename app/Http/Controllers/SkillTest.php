814fdcaf6e8d134d469e14853b1978037a1424a4
Zahran <muhammadzahran32@gmail.com>
2024-02-22 14:18:15 +0700

    Initial commit

---
+++ app/Http/Controllers/SkillTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SkillTest extends BaseController
+{
+    public function index()
+    {
+        // Jawaban no 01
+        $alphabet = 'aaabbcccddeddbzaa';
+        $compressedString = '';
+        $count = 1;
+
+        for ($i = 0; $i < strlen($alphabet); $i++) {
+            if ($i < strlen($alphabet) - 1 && $alphabet[$i] == $alphabet[$i + 1]) {
+                $count++;
+            } else {
+                $compressedString .= $alphabet[$i] . $count;
+
+                $count = 1;
+            }
+        }
+
+        echo $compressedString;
+        echo "<br>";
+        echo "--------------------------------------------------------------------------------";
+        echo "<br>";
+
+        //  Jawaban no 02
+        $numbers = [9, 3, 7, 8, 2, 6, 1, 4, 10, 2, 2, 3];
+        sort($numbers);
+
+        $arrLength = count($numbers);
+        for ($x = 0; $x < $arrLength; $x++) {
+            echo $numbers[$x];
+            echo "<br>";
+        }
+
+        return view('skill-test/views');
+    }
+
+    public function hitungBelanjaan($input)
+    {
+        $jenisBarang = $input->post('jenis_barang');
+        $jumlahBarang = $input->post('jumlah_barang');
+    }
+}
---
+++ resources/views/skill-test/views.blade.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Test Laravel</title>
+</head>
+<body>
+<form action="" method="post">
+    <p>Jenis Barang</p>
+    <input type="text" name="jenis_barang">
+    <br>
+    <p>Jumlah Barang</p>
+    <input type="text" name="jumlah_barang">
+    <br>
+    <button type="submit">Hitung</button>
+    <br>
+    <p>Total Harga : </p>
+</form>
+</body>
+</html>
